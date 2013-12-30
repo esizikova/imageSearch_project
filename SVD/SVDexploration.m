5dcc21b8fcf49d55a03de77563c51c3c141f1d0c
englehardt <englehardt@gmail.com>
2013-12-30 16:37:14 -0500

    [SVD] split into descriptor functions, new descriptors, and b&w support for all

--- SVD/SVDexploration.m
+++ SVD/SVDexploration.m
@@ -3,67 +3,68 @@
 
 %read in all images
 [images_set, labels] = loadImages( '../Dataset/' );
-noOfCat = 8; % for now i just use 2 categories
+noOfCat = 8;
 
 %Move from cells to 4-D matrices (yes, don't complain)
 for i = 1:length(images_set)
     images(:,:,:,i) = images_set{i};
 end
 
-%SVD - each color channel
+%SVD - compression
 for i = 1:size(images,4)
-    [U(:,:,1,i),S(:,:,1,i),V(:,:,1,i)] = svd(double(images(:,:,1,i)));
-    [U(:,:,2,i),S(:,:,2,i),V(:,:,2,i)] = svd(double(images(:,:,2,i)));
-    [U(:,:,3,i),S(:,:,3,i),V(:,:,3,i)] = svd(double(images(:,:,3,i)));
+    [U(:,:,:,i),S(:,:,:,i),V(:,:,:,i)] = imageSVD(images(:,:,:,i));
 
     %Reconstruct from less singular values
-    num_sv = 10;
-    out_img(:,:,1,i) = U(:,1:num_sv,1,i)*S(1:num_sv,1:num_sv,1,i)*V(:,1:num_sv,1,i)';
-    out_img(:,:,2,i) = U(:,1:num_sv,2,i)*S(1:num_sv,1:num_sv,2,i)*V(:,1:num_sv,2,i)';
-    out_img(:,:,3,i) = U(:,1:num_sv,3,i)*S(1:num_sv,1:num_sv,3,i)*V(:,1:num_sv,3,i)';
+    %num_sv = 10;
+    %out_img(:,:,1,i) = U(:,1:num_sv,1,i)*S(1:num_sv,1:num_sv,1,i)*V(:,1:num_sv,1,i)';
+    %out_img(:,:,2,i) = U(:,1:num_sv,2,i)*S(1:num_sv,1:num_sv,2,i)*V(:,1:num_sv,2,i)';
+    %out_img(:,:,3,i) = U(:,1:num_sv,3,i)*S(1:num_sv,1:num_sv,3,i)*V(:,1:num_sv,3,i)';
 
-    imshow(uint8(out_img(:,:,:,i)))
+    %imshow(uint8(out_img(:,:,:,i)))
 end
 
-%SVD - Just look at singular values
+%% SVD - Just look at singular values
+num_sv = 10;
 for i = 1:size(images,4)
     for j = 1:size(images,4)
-        desc_1 = cat(1,diag(S(:,:,1,i)),diag(S(:,:,2,i)),diag(S(:,:,3,i)));
-        desc_2 = cat(1,diag(S(:,:,1,j)),diag(S(:,:,2,j)),diag(S(:,:,3,j)));
-        eucl_dist = norm(desc_1 - desc_2);
-        sv_dist(i,j) = eucl_dist;
+        desc_1 = SVDSingularValue(images(:,:,:,i), num_sv, true, U(:,:,:,i), S(:,:,:,i), V(:,:,:,i));
+        desc_2 = SVDSingularValue(images(:,:,:,j), num_sv, true, U(:,:,:,j), S(:,:,:,j), V(:,:,:,j));
+        sv_dist(i,j) = norm(desc_1 - desc_2);
     end
 end
 
 % show the distance matrix nicely
 plotMatrix ( sv_dist, labels );
 
-%SVD - Stack slices of U and V with 1 singular value
+%% SVD - Stack slices of U and V
+num_ev = 3;
 for i = 1:size(images,4)
     for j = 1:size(images,4)
-        red = S(1,1,1,i)*cat(1,U(:,1,1,i),V(:,1,1,i));
-        green = S(1,1,2,i)*cat(1,U(:,1,2,i),V(:,1,2,i));
-        blue = S(1,1,3,i)*cat(1,U(:,1,3,i),V(:,1,3,i));
-        desc_1 = cat(1,red,green,blue);
-        
-        red = S(1,1,1,j)*cat(1,U(:,1,1,j),V(:,1,1,j));
-        green = S(1,1,2,j)*cat(1,U(:,1,2,j),V(:,1,2,j));
-        blue = S(1,1,3,j)*cat(1,U(:,1,3,j),V(:,1,3,j));
-        desc_2 = cat(1,red,green,blue);
-        
-        eucl_dist = norm(desc_1 - desc_2);
-        slice_dist(i,j) = eucl_dist;
+        desc_1 = SVDEVStacked(images(:,:,:,i), num_ev, true, U(:,:,:,i), S(:,:,:,i), V(:,:,:,i));
+        desc_2 = SVDEVStacked(images(:,:,:,j), num_ev, true, U(:,:,:,j), S(:,:,:,j), V(:,:,:,j)); 
+        slice_dist(i,j) = norm(desc_1 - desc_2);
     end
 end
 
 % show the distance matrix nicely
 plotMatrix ( slice_dist, labels );
 
-%--------------------------------------------------
-% k-means clustering
-dataPts = zeros( noOfCat*3 , 768);
-for i = 1:noOfCat*3
-    dataPts(i,:) = cat(1,diag(S(:,:,1,i)),diag(S(:,:,2,i)),diag(S(:,:,3,i)))';
+%% SVD - Main diagonal of resulting compressed image
+n_sv = 100;
+for i = 1:size(images,4)
+    for j = 1:size(images,4)
+        desc_1 = SVDDiagonal(images(:,:,:,i), num_sv, true, U(:,:,:,i), S(:,:,:,i), V(:,:,:,i));
+        desc_2 = SVDDiagonal(images(:,:,:,j), num_sv, true, U(:,:,:,j), S(:,:,:,j), V(:,:,:,j)); 
+        diag_dist(i,j) = norm(desc_1 - desc_2);
+    end
+end
+
+% show the distance matrix nicely
+plotMatrix ( diag_dist, labels );
+
+%% k-means clustering
+for i = 1:size(images,4)
+    dataPts(i,:) = SVDDiagonal(images(:,:,:,i), 100, true, U(:,:,:,i), S(:,:,:,i), V(:,:,:,i))';
 end;
 
 [clusterIdx, ctrs] = kmeans( dataPts, noOfCat, ...
@@ -84,7 +85,7 @@ for i = 1 : noOfCat
 end;
 
 
-
+%% Old stuff
 %Normalize back to uint8
 %red = (red - min(min(red)));
 %red = red*255/max(max(red));
