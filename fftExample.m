1b09ac3a9d2a156843f5b626776049138d146d3c
esizikova <elena.sizikova@gmail.com>
2014-01-07 12:42:01 -0800

    added basic fft descriptors

--- fftExample.m
+++ fftExample.m
@@ -1,9 +1,9 @@
 close all;
 clear all;
 
-im = imread ( 'Dataset/coast_bea1.jpg' );
+im = imread ( '/home/lena/Documents/CS_Projects/imageSearch_project/trunk/Dataset/insidecity_a248032.jpg' );
 im = double(im) / 255;
-%im = rgb2gray(im);  % 256*256
+im = rgb2gray(im);  % 256*256
 
 im_fft = fft2(im);  % 256*256
 imshow(fftshift(im_fft));
@@ -13,15 +13,19 @@ imshow(fftshift(im_fft));
 im_phase = angle (im_fft );
 im_amplitude = abs( im_fft );
 % im_phase = double(rand(256, 256));
-im_amplitude = double(rand(256, 256));
+%im_amplitude = double(rand(256, 256));
 im_fft2 = im_amplitude.*exp(i*im_phase);
 
 im_reconstruct = ifft2( im_fft2 );
 
 
-subplot(2,2,1), imshow( im,[] ), title ('orginal image');
+subplot(2,2,1), imshow( im,[] ), title ('original image');
 subplot(2,2,2), imshow( fftshift(im_phase),[] ), title ('phase image');
 % peak frequencies, are very high, so to see any useful infromation when
 % visualizing, take the log of the amplitude
 subplot(2,2,3), imshow( log(fftshift(im_amplitude)),[] ), title ('amplitude image');
 subplot(2,2,4), imshow( im_reconstruct,[] ), title ('reconstructed image');
+
+%- investigate patch match algorithm
+%- look at rotation invariance: how do we actually do rotations on images '
+%-  
